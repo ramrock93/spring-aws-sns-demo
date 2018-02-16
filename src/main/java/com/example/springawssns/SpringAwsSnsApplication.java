d3f1cfb43ab9b0f58f326aa0373504e53fd720cb
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2018-02-16 19:42:21 +0100

    initial commit

---
+++ src/main/java/com/example/springawssns/SpringAwsSnsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springawssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsSnsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springawssns/SpringAwsSnsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springawssns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAwsSnsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
